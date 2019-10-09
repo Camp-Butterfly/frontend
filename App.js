29adc34fa1ae9cb2068d0cdc7228363fb1d6c605
Michael Nurilov <michaelnurilov67@gmail.com>
2019-10-08 20:23:48 -0400

    Display picture just taken

--- App.js
+++ App.js
@@ -1,33 +1,36 @@
 'use strict';
-import React, { PureComponent } from 'react';
-import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
+/*let pic = {
+  uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+};*/
+
 class App extends React.Component {
+  state = {
+    uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+  }
   render() {
     return (
       <View style={styles.container}>
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <Image source={this.state} style={{width: 193, height: 110}}/>
+        </View>
         <RNCamera
           ref={ref => {
             this.camera = ref;
           }}
           style={styles.preview}
+          // Specifies that we are using the back camera
           type={RNCamera.Constants.Type.back}
+          // Determines if we are using flash
           flashMode={RNCamera.Constants.FlashMode.off}
           androidCameraPermissionOptions={{
             title: 'Permission to use camera',
             message: 'We need your permission to use your camera',
             buttonPositive: 'Ok',
             buttonNegative: 'Cancel',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
           }}
         />
         <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
@@ -39,11 +42,16 @@ class App extends React.Component {
     );
   }
 
+  //portrait", "portraitUpsideDown", "landscapeLeft" or "landscapeRight
   takePicture = async() => {
     if (this.camera) {
-      const options = { quality: 0.5, base64: true };
+      const options = { quality: 1, base64: true, orientation: "portraitUpsideDown" };
       const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
+      this.setState({
+        uri: data.uri
+      });
+      console.log("uri: ", data.uri);
+      console.log("Picture Orientation: ", data.pictureOrientation);
     }
   };
 }
@@ -71,5 +79,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-
-AppRegistry.registerComponent('App', () => App);
